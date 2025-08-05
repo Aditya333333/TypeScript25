568e694847e712884ba1a0b74abe5d40943280c8
Aditya <adityaraja171102@gmail.com>
2025-08-06 00:21:36 +0530

    DataTypes

---
+++ into.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// String
+var Name = "Aditya";
+console.log(Name);
+Name = "Rahul";
+//number
+var num = 445;
+console.log(num);
+num = 345.6;
+console.log(num);
+console.log(typeof num);
+//boolean
+var bol = true;
+console.log(bol);
+//null
+var nul = null;
+console.log(nul);
+//undefinded
+var und = undefined;
+console.log(und);
---
+++ into.ts
@@ -0,0 +1,34 @@
+
+// String
+let Name : string = "Aditya"
+console.log(Name);
+Name = "Rahul"
+
+
+//number
+let num : number = 445
+console.log(num);
+num = 345.6
+console.log(num);
+console.log(typeof num);
+
+
+
+
+//boolean
+let bol : boolean = true
+console.log(bol);
+
+//null
+let nul:null= null
+console.log(nul);
+
+//undefinded
+let und :undefined = undefined
+console.log(und);
+
+
+export {}
+
+
+
